ff1558c5d2a9aeb1e3f24495a45909cdc752c659
andrey <a.serbin@litebox.ru>
2021-05-26 19:32:56 +0400

    добавлены две активити и обновлена основная

--- app/src/main/java/com/example/lessonone/MainActivity.java
+++ app/src/main/java/com/example/lessonone/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lessonone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickEms(View view) {
+        Intent intent = new Intent(this, ThirdActivity.class);
+        startActivity(intent);
     }
 }
